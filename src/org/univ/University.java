bcd675b1ba6c6f8450932e25ea7aa0553405037c
Devaaaga <devaagar914@gmail.com>
2024-11-04 16:50:43 +0530

    done

---
+++ src/org/univ/University.java
@@ -0,0 +1,12 @@
+package org.univ;
+//method overriding/Dynamic Binding
+//Runtime polymorphism
+public class University {
+	public void ug() {
+		System.out.println("ug:Bachelor of arts");
+	}
+	public void pg() {
+		System.out.println("pg:Master of arts");
+	}
+
+}
